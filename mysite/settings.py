ee1afca274386041640d3797fb58904cab35561a
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2024-11-26 00:05:48 +0900

    context: ./quiz/に修正

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,9 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/app/static'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = '/app/
