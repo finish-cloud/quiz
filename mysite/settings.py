45c7cfaf0d23ae988885d16862eeec409fb8b72a
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2024-11-26 20:27:30 +0900

    nginx.confを修正

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,4 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/app/
+STATIC_ROOT = '/app/static'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
