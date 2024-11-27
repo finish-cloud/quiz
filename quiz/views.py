e5f50447b392e2ebf955f7673c135734db95c95a
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2024-11-27 23:19:32 +0900

    docker-compose.ymlにfrontendを追加

--- quiz/views.py
+++ quiz/views.py
@@ -2,14 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Question
-from .serializers import QuestionSerializer
-
-def index(request):
-    return HttpResponse ("Hello, Django!")
-
-class QuizView(APIView):
-    def get(self, request):
-        questions = Question.objects.all()
-        serializer = QuestionSerializer(questions, many=True)
-        return Response(serializer)
+from
