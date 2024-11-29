4139b89afee85cb2788a2ebdd2502fb741f59beb
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2024-11-29 23:34:13 +0900

    docker-compose.ymlのfrontend部分を修正

--- quiz-frontend/src/App.js
+++ quiz-frontend/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/api/quiz").then((response) => {
+      setQuestions(response.data);
+    });
+  }, []);
+  const handleAnswer = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestionIndex = currentQuestion + 1;
+    if (nextQuestionIndex < questions.length) {
+      setCurrentQuestionIndex(nextQuestionIndex);
+    } else {
+      setShowScore(true);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+        </div>
+      ) : (
+        questions.length > 0 && (
+          <div className="question-section">
+            <div className="question-text">
+              {questions[currentQuestionIndex].text}
+            </div>
+            <div className="answer-section">
+              {questions[currentQuestionIndex].choices.map((choice) => (
+                <button
+                  key={choice.id}
+                  onClick={() => handleAnswerClick(choice.is_correct)}
+                >
+                  {choice.text}
+                </button>
+              ))}
+            </div>
+          </div>
+        )
+      )}
     </div>
   );
 }
