ba44ae0560d401d454c44624796c195cf2f30371
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2024-12-03 23:31:06 +0900

    STATICFILES_DIRSを追加

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,6 +144,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = '/app/static'
 
+STATICFILES_DIRS = [BASE_DIR / "quiz-frontend/build/static"]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
