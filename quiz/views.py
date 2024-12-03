fb63ab17542dc1a507c4d6112f667c2ddac58878
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2024-12-03 23:30:10 +0900

    React の HTML を Django で提供

--- quiz/views.py
+++ quiz/views.py
@@ -5,11 +5,13 @@ from rest_framework.response import Response
 from .models import Question
 from .serializers import QuestionSerializer
 
+
 def index(request):
-    return HttpResponse ("Hello, Django!")
+    return render(request, "build/index.html")
 
 class QuizView(APIView):
     def get(self, request):
         questions = Question.objects.all()
         serializer = QuestionSerializer(questions, many=True)
         return Response(serializer.data)
+
