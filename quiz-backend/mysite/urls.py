4b2cc4666ee29b98e8c6360b0b5f0878eda5c022
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2024-12-14 13:51:02 +0900

    mysiteのurls.pyのテスト部分を修正

--- quiz-backend/mysite/urls.py
+++ quiz-backend/mysite/urls.py
@@ -16,13 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Welcome to Django backend!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('quiz/', include('quiz.urls')), # quizアプリのルートをインクルード
+    path('quiz/', include('quiz.urls')), #追加
     path('', index, name='index'),  # ルートURL
 ]
