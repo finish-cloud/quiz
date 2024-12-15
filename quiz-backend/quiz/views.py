69992b70642d2c88239064e38e40aa4e877449f8
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2024-12-15 11:28:24 +0900

    クイズの問題と解説を分けるようにする

--- quiz-backend/quiz/views.py
+++ quiz-backend/quiz/views.py
@@ -9,8 +9,17 @@ from .serializers import QuestionSerializer
 def index(request):
     return render(request, "build/index.html")
 
-class QuizView(APIView):
+class QuestionListView(APIView):
     def get(self, request):
-        questions = Question.objects.all()
+        questions = Question.objects.prefetch_related('choice', 'explanation').all()
         serializer = QuestionSerializer(questions, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+class QuestionDetailView(APIView):
+    def get(self, request, pk):
+        try:
+            question = Question.objects.prefetch_related('choice', 'explanation').get(pk=pk)
+        except Question.DoesNotExist:
+            return
+        serializer = QuestionSerializer(question)
+        return Response(serializer.data, status=status.HTTP_200_OK)
