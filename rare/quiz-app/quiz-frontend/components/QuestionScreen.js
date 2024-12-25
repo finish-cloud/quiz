ffb7b99e919910d74a4eba379c362a2ce076540d
finish-cloud <hope.is.a.waking.dream.8.1@gmail.com>
2024-12-25 23:46:38 +0900

    選択肢を追加

--- rare/quiz-app/quiz-frontend/components/QuestionScreen.js
+++ rare/quiz-app/quiz-frontend/components/QuestionScreen.js
@@ -16,6 +16,29 @@ const QuestionScreen = ({ questionDate, onAnswerSubmit }) => {
       <div className="card-shadow">
         <div className="card-body">
           <h4 className="card-title text-center">{questionData.text}</h4>
+          <ul className="list-group">
+            {questionData.choices.map((choice, index) => (
+              <li
+                key={index}
+                className={`list-group-item ${
+                  selectedChoice === choice.value? "active" : ""
+                }`}
+                onClick={() => handleSelectCHoice(choice)}
+              >
+                {choice.text}
+              </li>
+            )) }
+          </ul>
+          <div className="text-center">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={handleSubmit}
+              disabled={!selectedChoice}
+            >
+              回答する
+            </button>
+          </div>
         </div>
       </div>
     </div>
